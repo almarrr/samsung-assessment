54eb4d6ec55343ce1c807119799fe0a3e5fe935b
Almar Groenewald <almarrr@gmail.com>
2023-07-29 19:25:27 +0200

    added product to custom usemodel hook

--- src/hooks/useModel.tsx
+++ src/hooks/useModel.tsx
@@ -2,14 +2,22 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { STORE } from "../store/store";
 import { useCallback, useEffect } from "react";
 
-export const useModel = (modelCode: string | null) => {
+interface IParams {
+  modelCode?: string;
+}
+
+export const useModel = (params: IParams) => {
+  const { modelCode } = params;
+
   const products = useRecoilValue(STORE.products);
 
   const [model, setModel] = useRecoilState(STORE.model);
 
+  const [product, setProduct] = useRecoilState(STORE.product);
+
   const findModel = useCallback(
-    (modelCode: string | null) => {
-      if (!modelCode) return;
+    (modelCode: string) => {
+      if (!modelCode || !products.length) return;
 
       const model =
         products
@@ -18,19 +26,27 @@ export const useModel = (modelCode: string | null) => {
           })
           .filter((x) => x !== undefined)[0] || null;
 
+      const product = products.find((x) => {
+        return x.modelList.find((x) => x.modelCode === modelCode);
+      });
+
+      if (product) {
+        setProduct(product);
+      }
+
       if (model) {
         setModel(model);
         return;
       }
-
-      console.log(`model ${modelCode} not found in products`);
     },
-    [products, setModel]
+    [products, setModel, setProduct]
   );
 
   useEffect(() => {
+    if (!modelCode) return;
+
     findModel(modelCode);
   }, [findModel, modelCode]);
 
-  return { model, setModel };
+  return { model, product };
 };
