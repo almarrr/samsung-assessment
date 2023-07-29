7ef70f6d37950a26654ea30950ccfc3c63d5dbce
Almar Groenewald <almarrr@gmail.com>
2023-07-29 18:02:15 +0200

    added footer to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import ProductGrid from "./pages/ProductGrid";
 import "./fonts/samsung-fonts.css";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <ProductGrid />;
+  return (
+    <>
+      <ProductGrid />
+      <Footer theme="light" />
+    </>
+  );
 };
 
 export default App;
