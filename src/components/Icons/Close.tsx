dadb798062ec52b2b8bdef16bd9242c334c5bdfe
Almar Groenewald <almarrr@gmail.com>
2023-07-29 12:29:51 +0200

    added close icon

---
+++ src/components/Icons/Close.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from "./_Icons";
+
+const Close: React.FC<IconProps> = ({ strokeWidth = 1.5 }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke="currentColor"
+      className="w-8 h-8"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
+
+export default Close;
---
+++ src/components/Icons/_Icons.tsx
@@ -0,0 +1,9 @@
+import Close from "./Close";
+
+export interface IconProps {
+  strokeWidth?: number;
+}
+
+export const Icons = {
+  Close,
+};
