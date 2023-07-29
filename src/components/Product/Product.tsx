3eb2c0989fcbdc5c301206a5d316b51c84d7fcf1
Almar Groenewald <almarrr@gmail.com>
2023-07-29 16:59:17 +0200

    updated product component view

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { ModelList, ProductList } from "../../types/types";
 import ChipOptions from "../ChipOptions/ChipOptions";
+import Button from "../Button";
+import { useSetRecoilState } from "recoil";
+import { STORE } from "../../store/store";
 
 const Product: React.FC<ProductList> = (props) => {
-  const [modelCode, setModelCode] = useState(props.modelList[0].modelCode);
+  const [modelCode] = useState<string | null>(
+    props.modelList[0].modelCode || null
+  );
+
+  const setActiveModel = useSetRecoilState(STORE.activeModelCode);
 
   const [model, setModel] = useState<null | ModelList>(null);
+
+  const toggleActiveModel = () => {
+    setActiveModel(modelCode);
+  };
 
   useEffect(() => {
     const result = props.modelList.find((model) => {
@@ -26,22 +37,29 @@ const Product: React.FC<ProductList> = (props) => {
   }
 
   return (
-    <div className="bg-white rounded-lg relative">
-      <div className="absolute top-[1rem] text-[.8rem] right-[1rem] bg-[#2189ff] rounded-md py-[.1.75rem] text-white font-bold px-1 z-[3]">
-        {props.modelList.length} models
+    <div className="bg-white rounded-2xl relative flex flex-col justify-between">
+      <div>
+        <button className="p-8" onClick={toggleActiveModel}>
+          <img
+            className="aspect-[16/10] object-contain w-full"
+            src={model.thumbUrl}
+            alt={model.displayName}
+          />
+        </button>
+        <div className="p-4">
+          <h3 className="text-[16px] font-bold">{model.displayName}</h3>
+          {props.chipOptions?.map((chip) => (
+            <ChipOptions key={chip.fmyChipType} {...chip} />
+          ))}
+        </div>
+        <ul className="px-8 pb-4 list-disc text-[14px] grid grid-cols-1 gap-2">
+          {model.usp.slice(0, 2).map((usp) => (
+            <li key={usp}>{usp}</li>
+          ))}
+        </ul>
       </div>
-      <div className="p-8">
-        <img
-          className="aspect-square object-cover w-full"
-          src={model.thumbUrl}
-          alt={model.displayName}
-        />
-      </div>
-      <div className="p-4">
-        <h3 className="text-[16px] font-bold">{model.displayName}</h3>
-        {props.chipOptions?.map((chip) => (
-          <ChipOptions key={chip.fmyChipType} {...chip} />
-        ))}
+      <div className="px-4 pb-8 pt-2 flex justify-center">
+        <Button label="Ontdek" onClick={toggleActiveModel} />
       </div>
     </div>
   );
