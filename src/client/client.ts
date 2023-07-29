1e71965eb6b670e8253273e9c33fd55a2dc3d387
Almar Groenewald <almarrr@gmail.com>
2023-07-29 12:00:46 +0200

    made client static

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,17 +1,20 @@
 import { SamsungResult, ProductList } from "../types/types";
 
 export class SamsungClient {
+    private static apiUrl = import.meta.env.VITE_API_URL;
 
-    async fetchProducts(): Promise<ProductList[]> {
+    private static async fetchJson<T>(url: string): Promise<T> {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(response.statusText);
+        return response.json();
+    }
+
+    static async fetchProducts(): Promise<ProductList[]> {
         try {
-            const result = await fetch(import.meta.env.VITE_API_URL);
-
-            const { response }: SamsungResult = await result.json();
-
+            const { response }: SamsungResult = await this.fetchJson(this.apiUrl);
             return response.resultData.productList;
-        }
-        catch (error: unknown) {
-            console.log(error);
+        } catch (error) {
+            console.error(error);
             throw error;
         }
     }
