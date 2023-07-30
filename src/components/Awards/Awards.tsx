2e82a623a399650a6cc034f7665c5b046c9dad69
Almar Groenewald <almarrr@gmail.com>
2023-07-30 19:23:45 +0200

    awards update

--- src/components/Awards/Awards.tsx
+++ src/components/Awards/Awards.tsx
@@ -12,27 +12,24 @@ const Awards: React.FC<IProps> = ({ modelCode }) => {
 
   return (
     <div className="flex justify-center items-center space-x-4 pt-60">
-      {product.awardList.map(
-        ({ awardTitle, awardDesc, awardLink, awardImg }) => (
-          <a
-            href={awardLink}
-            target="_blank"
-            className="block relative bg-samsung-light/10 hover:bg-samsung-light/20 transition-all duration-150 w-1/3 px-10 py-12 rounded-lg"
-            rel="noreferrer"
-          >
-            <h3>{awardTitle}</h3>
-            <p className="text-[16px] pt-3 text-center">{awardDesc}</p>
+      {product.awardList.map(({ awardTitle, awardLink, awardImg }) => (
+        <a
+          href={awardLink}
+          target="_blank"
+          className="block relative bg-samsung-light/10 hover:bg-samsung-light/20 transition-all duration-150 w-1/3 px-10 py-12 rounded-lg"
+          rel="noreferrer"
+        >
+          <h3>"{awardTitle}"</h3>
 
-            {awardImg && (
-              <img
-                src={awardImg}
-                alt={awardTitle}
-                className="block mx-auto pt-8 object-contain max-w-[100px]"
-              />
-            )}
-          </a>
-        )
-      )}
+          {awardImg && (
+            <img
+              src={awardImg}
+              alt={awardTitle}
+              className="block mx-auto pt-8 object-contain max-w-[100px]"
+            />
+          )}
+        </a>
+      ))}
     </div>
   );
 };
