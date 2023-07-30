3b21fdda894cf8534389af8dba008ef4bdecabe0
Almar Groenewald <almarrr@gmail.com>
2023-07-30 16:52:49 +0200

    modded gridprops with modelcode instead of product

--- src/pages/ProductGrid.tsx
+++ src/pages/ProductGrid.tsx
@@ -9,7 +9,10 @@ const ProductGrid = () => {
     <section className="py-8">
       <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {products.map((product) => (
-          <Product key={product.familyId} {...product} />
+          <Product
+            key={product.familyId}
+            modelCode={product.modelList[0].modelCode}
+          />
         ))}
       </div>
     </section>
